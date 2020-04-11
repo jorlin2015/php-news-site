a349cd6c6088732ee14a53f103f9a71ef77013cb
zhangchaolin <zhangchaolin@okay.cn>
2020-04-12 02:10:39 +0800

    不知道为啥改的

--- extend/logAnalysis/DayLogFile.php
+++ extend/logAnalysis/DayLogFile.php
@@ -4,25 +4,27 @@
 	/**
 	 *多线程分析每天日志
 	 */
-	class DayLogFile extends \Thread
+	class DayLogFile/* extends \Thread*/
 	{
 		private static $paths = [
-			'/Users/zhangchaolin/Documents/study/php/log-238/localhost_access_log..',
+			// '/Users/zhangchaolin/Documents/study/php/log-238/localhost_access_log..',
 			'/Users/zhangchaolin/Documents/study/php/log-15/localhost_access_log.'
 		];
 		function __construct($fileName, $result)
 		{
 			$this->fileName = $fileName;
 			$this->result = $result;
-		}
-		public function run(){
-			// var_dump($this->result);
 			$this->result[$this->fileName] = $this->getDayActives();
 		}
+		// public function run(){
+			// var_dump($this->result);
+			// $this->result[$this->fileName] = $this->getDayActives();
+		// }
 		private function getDayActives(){
 			$result = [];
 			foreach (self::$paths as $index => $path) {
 				$fileName = $path.$this->fileName.'.txt';
+				// var_dump($fileName);
 				$res = file_get_contents($fileName);
 				preg_match_all('/images\/o.gif\?(.*action=page-access.*) HTTP\/1.1"/',$res,$matches);
 				foreach ($matches[1] as $key => $value) {
--- extend/logAnalysis/LogAnalysis.php
+++ extend/logAnalysis/LogAnalysis.php
@@ -2,54 +2,66 @@
 namespace logAnalysis;
 
 require_once('DayLogFile.php');
-require_once('StorageData.php');
+// require_once('StorageData.php');
 use logAnalysis\DayLogFile;
-use logAnalysis\StorageData;
+// use logAnalysis\StorageData;
 /**
 * 统计教师空间日活,月活
 */
 class LogAnalysis
 {
-	private $pSize = 5;
-	private $pools = [];
+	// private $pSize = 5;
+	// private $pools = [];
 	function __construct($start, $end){
 		$this->start = $start;
 		$this->end = $end;
-		$this->result = new StorageData();
+		$this->result = [];
+		// $this->result = new StorageData();
 	}
 	public function analysisByDay(){
 		$dates = $this->getDays();
 		$this->days = count($dates);
-		$this->enterPools($dates);
+		// $this->enterPools($dates);
 		//线程池没满就新建线程加入线程池,否则等待线程池有空闲
 		while (count($dates) > 0) {
-			foreach ($this->pools as $key => $worker) {
-				if(!$worker->isrunning()){
+			// foreach ($this->pools as $key => $worker) {
+				// if(!$worker->isrunning()){
 					$fileName = array_shift($dates);
-					$pools[$key] = new DayLogFile($fileName, $this->result);
-					$pools[$key]->start();
-				}
-			}
-			usleep(1000);
+					/*$pools[$key] = */new DayLogFile($fileName, $this->result);
+					// $pools[$key]->start();
+				// }
+			// }
+			// usleep(1000);
 		}
 		$sum = 0;
 		foreach ($this->result as $key => $value) {
-			foreach ($value as $key1 => $value1) {
+			/*foreach ($value as $key1 => $value1) {
 				$sum += $value1;
-			}
+			}*/
+			$sum += count($value);
 		}
 		/*var_dump($sum);
 		var_dump($this->days);*/
 		var_dump(round($sum / $this->days));
 	}
-	private function enterPools($dates){
-		$size = $this->pSize > $this->days ? $this->pSize : $this->days;
-		if(count($this->pools) < $size){
-			$fileName = array_shift($dates);
-			$work = new DayLogFile($fileName, $this->result);
-			array_push($this->pools, $work);
-			$work->start();
+	// private function enterPools($dates){
+	// 	$size = $this->pSize > $this->days ? $this->pSize : $this->days;
+	// 	if(count($this->pools) < $size){
+	// 		$fileName = array_shift($dates);
+	// 		$work = new DayLogFile($fileName, $this->result);
+	// 		array_push($this->pools, $work);
+	// 		$work->start();
+	// 	}
+	// }
+	public function analysiByMonth(){
+		$uniq = [];
+		foreach ($this->result as $key => $value) {
+			foreach ($value as $key1 => $value1) {
+				array_push($uniq, $value1);
+			}
 		}
+		$uniq = array_unique($uniq);
+		var_dump(count($uniq));
 	}
 	private function getDays(){
 		$date1 = date_create($this->start);
@@ -64,6 +76,7 @@ class LogAnalysis
 		return $result;
 	}
 }
-$obj = new LogAnalysis('2019-01-01', '2019-02-16');
+$obj = new LogAnalysis('2019-01-01', '2019-01-31');
 $obj->analysisByDay();
+$obj->analysiByMonth();
 ?>
